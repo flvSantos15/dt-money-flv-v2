7f90f85b908966df6235f507508d1c0c31aceb58
flvSantos15 <flvsantos300@gmail.com>
2022-11-15 15:07:18 -0300

    feat(new-transaction-modal): since we need the transaction information, a modal needed to be created

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'phosphor-react'
+
+import { CloseButton, Content, Overlay } from './styles'
 
 export function NewTransactionModal() {
-  return (
-    <Dialog.Portal>
-      <Dialog.Overlay>
-        <Dialog.Content>
-          <Dialog.Title>Nova Transação</Dialog.Title>
+	return (
+		<Dialog.Portal>
+			<Overlay>
+				<Content>
+					<Dialog.Title>Nova Transação</Dialog.Title>
 
-          <Dialog.Close />
-        </Dialog.Content>
-      </Dialog.Overlay>
-    </Dialog.Portal>
-  )
+					<CloseButton>
+						<X size={24} />
+					</CloseButton>
+
+					<form action="">
+						<input type="text" placeholder="Descrição" required />
+						<input type="number" placeholder="Preço" required />
+						<input type="text" placeholder="Categoria" required />
+
+						<button type="submit">Cadastrar</button>
+					</form>
+				</Content>
+			</Overlay>
+		</Dialog.Portal>
+	)
 }
